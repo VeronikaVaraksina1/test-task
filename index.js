acd451fe1934d1944a42c7f0c401e28604b2a185
VeronikaVaraksina1 <plaguemoon@gmail.com>
2024-09-30 12:56:03 +0300

    delete comment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const banner = document.querySelector(".banner");
 const closeBtn = document.querySelector(".close-button");
 
-closeBtn.addEventListener('click', () => {
-    banner.classList.add('hidden');
-})
+closeBtn.addEventListener("click", () => {
+  banner.classList.add("hidden");
+});
